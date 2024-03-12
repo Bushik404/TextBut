289f44124170efbe2536392dae601e043e816036
Bushik404 <bushuev2002-2013@mail.ru>
2024-03-12 12:28:25 +0300

    f

---
+++ untitled/src/main/java/org/example/TextBut.java
@@ -0,0 +1,62 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextBut extends JFrame implements Runnable{
+
+    @Override
+    public void run() {
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BorderLayout());
+
+        JTextArea textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        jPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        JButton add = new JButton("Добавить");
+        JButton clear = new JButton("Очистить");
+        JButton exit = new JButton("Выход");
+        Dimension buttonSize = new Dimension(100, 30);
+        add.setPreferredSize(buttonSize);
+        clear.setPreferredSize(buttonSize);
+        exit.setPreferredSize(buttonSize);
+
+
+        clear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.setText("");
+            }
+        });
+
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        buttonPanel.add(add);
+        buttonPanel.add(clear);
+        buttonPanel.add(exit);
+        jPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        add(jPanel);
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(500, 500, 500, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        TextBut tst = new TextBut();
+        Thread thread = new Thread(tst);
+        thread.start();
+    }
+}
