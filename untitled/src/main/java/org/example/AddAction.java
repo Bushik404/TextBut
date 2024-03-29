97c63dd72c546fbc0ff123b965ec457438fb45a1
Bushik404 <bushuev2002-2013@mail.ru>
2024-03-29 17:59:25 +0300

    13

--- untitled/src/main/java/org/example/AddAction.java
+++ untitled/src/main/java/org/example/AddAction.java
@@ -1,8 +1,9 @@
 package org.example;
+
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
-
+import java.util.concurrent.*;
 
 public class AddAction implements ActionListener {
     private JTextArea textArea;
@@ -16,22 +17,40 @@ public class AddAction implements ActionListener {
         int returnValue = fileChooser.showOpenDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(selectedFile));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line).append("\n");
-                    try {
+
+            Future<IOException> exceptionFuture = new FutureTask<>(() -> {
+                IOException ioException = null;
+                try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    int lineCounter = 0;
+                    while ((line = reader.readLine()) != null) {
+                        lineCounter++;
+                        stringBuilder.append(line).append("\n");
+                        if (lineCounter == 5) {
+                            throw new IOException("Пятая строка");
+                        }
                         Thread.sleep(3000);
-                    } catch (InterruptedException ex) {
-                        // Обработка прерывания потока
-                        ex.printStackTrace();
                     }
+                    SwingUtilities.invokeLater(() -> textArea.setText(stringBuilder.toString()));
+                } catch (IOException ex) {
+                    ioException = ex;
                 }
-                reader.close();
-                textArea.setText(stringBuilder.toString());
-            } catch (IOException ex) {
+                return ioException;
+            });
+
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.submit((Runnable) exceptionFuture);
+            executor.shutdown();
+
+            try {
+                IOException exception = exceptionFuture.get();
+                if (exception != null) {
+                    exception.printStackTrace();
+                } else {
+                    System.out.println("Файл успешно прочитан.");
+                }
+            } catch (InterruptedException | ExecutionException ex) {
                 ex.printStackTrace();
             }
         }
